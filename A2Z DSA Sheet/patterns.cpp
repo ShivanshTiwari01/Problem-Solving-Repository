c50c4b732fdb244d2177bbebfca437ebd21a075e
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2024-10-02 13:37:28 +0530

    update

--- A2Z DSA Sheet/patterns.cpp
+++ A2Z DSA Sheet/patterns.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Pattern 1
 int pattern1(int n) {
     for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++){
+        for(int j=0; j<n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int pattern2(int n) {
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++){
             cout << "*";
         }
         cout << endl;
@@ -15,7 +23,7 @@ int main() {
 
     int n;
     cin >> n;
-    pattern1(n);
+    pattern2(n);
     
     return 0;
 }
