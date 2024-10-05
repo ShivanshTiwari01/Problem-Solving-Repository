71ec9e3c5460ed22a5a3c33b3f50163792f1e277
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2024-10-05 23:16:33 +0530

    update

--- A2Z DSA Sheet/patterns.cpp
+++ A2Z DSA Sheet/patterns.cpp
@@ -46,12 +46,21 @@ int pattern5(int n) {
     }
 }
 
+int pattern6(int n) {
+    for (int i=n; i>0; i--) {
+        for (int j=1; j<=i; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
 
 int main() {
 
     int n;
     cin >> n;
-    pattern5(n);
+    pattern6(n);
     
     return 0;
 }
