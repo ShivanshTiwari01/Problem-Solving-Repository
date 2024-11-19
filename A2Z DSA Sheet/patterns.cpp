76fac415e7e655e6f5718094c83cfb0d1e8684f5
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2024-11-19 14:05:39 +0530

    new functions added

--- A2Z DSA Sheet/patterns.cpp
+++ A2Z DSA Sheet/patterns.cpp
@@ -113,17 +113,68 @@ int pattern9(int n) {
 }
 
 int pattern10(int n) {
-    for(int i=0; i<n; i++){
-        
+    for(int i=0; i<= 2*n-1; i++){
+        int stars = i;
+        if(i>n){
+            stars = 2*n-i;
+        }
+        for(int j=1; j<=stars; j++){
+            cout << "*";
+        }
+        cout << endl;
     }
 }
 
+int pattern11(int n){
+    int start = 1;
+    for(int i=0; i<n; i++){
+        if(i%2 == 0) start = 1;
+        else start = 0;
+        for(int j=0; j<=i; j++){
+            cout << start;
+            start = 1-start;
+        }
+        cout << endl;
+    }
+}
+
+int pattern12(int n){
+    int spaces = 2*(n-1);
+    for(int i=0; i<n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j;
+        }
+        for(int j=1; j<=spaces; j++){
+            cout << " ";
+        }
+        for(int j=i; j>=1; j--){
+            cout << j;
+        }
+        cout << endl;
+        spaces -= 2;
+    }
+}
+
+int pattern13(int n){
+    int t=1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout << t << " ";
+            t += 1;
+        }
+        cout << endl;
+    }
+}
+
+int pattern14(int n){
+
+}
 
 int main() {
 
     int n;
     cin >> n;
-    pattern10(n);
+    pattern14(n);
     
     return 0;
 }
