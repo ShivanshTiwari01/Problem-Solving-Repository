e0f0ec6a8887ca818a44ea54ee017a312c536e14
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2024-11-24 23:23:59 +0530

    update

---
+++ TUF A2Z DSA Sheet/basics/maths problems/armstrong_number.cpp
@@ -0,0 +1,40 @@
+// Program: Check if the given number is Armstrong or not. 
+// Armstrong numbers are the numbers whose sum of cubes of each digit is equal to the number itself eg 153 and 1634.
+
+# include <bits/stdc++.h>
+using namespace std;
+
+int power(int x, unsigned int y)
+{
+    if (y == 0)
+        return 1;
+    if (y % 2 == 0)
+        return power(x, y / 2) * power(x, y / 2);
+    return x * power(x, y / 2) * power(x, y / 2);
+}
+
+string check_armstrong(int n) {
+    
+    int digits = 0;
+    
+    int temp = n;
+    while(temp > 0) {
+        digits += 1;
+        temp /= 10;
+    }
+    temp = n;
+    int arm = 0;
+    
+    while(temp > 0) {
+        int num = temp%10;
+        arm += power(num, digits);
+        temp /= 10;
+    }
+    return arm == n ? "true" : "false";
+}
+
+int main() {
+    int n;
+    cin >> n; 
+    cout << "Given no is armstrong: " << check_armstrong(n);
+}
---
+++ TUF A2Z DSA Sheet/basics/maths problems/check_palindrome.cpp
@@ -0,0 +1,21 @@
+// Program to check if a given number is palindrome or not.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string check_palindrome(int n) {
+    int temp = n;
+    int rev = 0;
+    while(temp) {
+        rev = rev*10 + temp%10;
+        temp /= 10;
+    }
+    if(rev == n) return "true";
+    return "false";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << check_palindrome(n);
+}
