38754932c36cf5c7d75c78b1568432c45eebad15
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2024-12-12 16:02:33 +0530

    more programs added

---
+++ TUF A2Z DSA Sheet/Arrays/Easy Problems/longest_subarray_with_sum_K.cpp
@@ -0,0 +1,15 @@
+// Program: Find the Longest subarray with given sum K.
+
+// Approach one: Using Hash Map this approach works for positive and negatives both. Best approach for arrays containing positve, zeroes and negatives.
+
+
+// Approach two: Using Two pointer, this approach works only for positives including zeroes, but is more optimised approach.
+// Best approach for arrays containing only positives and zeroes.
+
+
+int main() {
+
+
+
+    return 0;
+}
